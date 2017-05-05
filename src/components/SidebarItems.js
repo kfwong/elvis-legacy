bb46f32177624ca08f561b73dbbe338ad4c93ed7
kfwong <kfwong@zoho.com>
2017-05-06 06:26:45 +0800

    Added hash link for router.

--- src/components/SidebarItems.js
+++ src/components/SidebarItems.js
@@ -1,14 +1,17 @@
 import React, {Component} from 'react';
 import {Menu} from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
 
 export default class SidebarItems extends Component {
     generateModuleMenuItems() {
         return this.props.modules.map(module => {
             return (
                 <Menu.Item>
-                    <h5>{module.code}<br/>
-                        <small>{module.name}</small>
-                    </h5>
+                    <Link to={'/'+module.code}>
+                        <h5>{module.code}<br/>
+                            <small>{module.name}</small>
+                        </h5>
+                    </Link>
                 </Menu.Item>
             );
         });
